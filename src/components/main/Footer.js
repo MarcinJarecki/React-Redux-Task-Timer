eeeb6db5fe23a4a94943a4621034a6c59dfbb65f
MarcinJarecki <username@users.noreply.github.com>
2017-05-13 15:26:04 +0200

    Add margin to footbar icon

--- src/components/main/Footer.js
+++ src/components/main/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
     render() {
         return (
             <div className="navbar navbar-default navbar-fixed-bottom">
-                <div className="container">
+                <div className="container footer-navbar">
                     <a href="https://github.com/MarcinJarecki" ><FontAwesome name='github' size='2x' /> </a>
                     <a href="https://pl.linkedin.com/in/marcinjarecki" ><FontAwesome name='linkedin-square' size='2x' /> </a>
                 </div>
